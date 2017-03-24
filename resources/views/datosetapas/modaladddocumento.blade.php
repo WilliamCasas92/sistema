4b1e108255614f4ebff3a5d2174fd933b80aed73
AppLaravel <juan_osorio82112@elpoli.edu.co>
2017-03-24 10:40:17 -0500

    ELimininación de los test que se realizaban para el componente subir documento

--- resources/views/datosetapas/modaladddocumento.blade.php
+++ resources/views/datosetapas/modaladddocumento.blade.php
@@ -13,9 +13,8 @@
                                   'files'=>'true',
                                   'id' => 'my-dropzone' ,
                                   'class' => 'dropzone']) !!}
+                <input type="hidden" name="_token" value="{{ csrf_field() }}">
                 <div class="dz-message needsclick" style="height:200px;">
-                    Drop your files here
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
                     <input type="hidden" id="modaladdDocumentoIdproceso" name="proceso_contractual_id" />
                     <input type="hidden" id="modaladdDocumentoIdrequisito" name="requisito_id" />
                     <input type="hidden" id="modaladdDocumentoIdetapa" name="etapa_id" />
